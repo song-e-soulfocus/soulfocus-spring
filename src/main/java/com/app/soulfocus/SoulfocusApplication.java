31cc04a589a1cc417aec7cf8af088ebdcd92ad67
taehee <taeheecafe@gmail.com>
2025-12-02 04:31:18 +0900

    soul focus Project

---
+++ src/main/java/com/app/soulfocus/SoulfocusApplication.java
@@ -0,0 +1,13 @@
+package com.app.soulfocus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoulfocusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoulfocusApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/app/soulfocus/SoulfocusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.soulfocus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoulfocusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
